c076357d696b6e45018614bcb8eacc9b01e72cf6
Jaap Mengers <jaapm@q42.nl>
2016-10-23 10:48:53 +0200

    Returns an observable containing the winner of the last match

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from __future__ import print_function
-import capture
+# import captureinputs as ci
+import captureframes as cf
 import predict
 
 try:
-    capture.get_event_summary() \
-        .map(predict.predict_player) \
-        .map(predict.player_for_score) \
+    cf.get_winners() \
         .subscribe(lambda e: print(e))
+
+
+    # ci.get_event_summary() \
+    #     .map(predict.predict_player) \
+    #     .map(predict.player_for_score) \
+    #     .subscribe(lambda e: print(e))
 except Exception as ex:
-    print("Test")
-    print(ex)
+    # print("Test")
+    # print(ex)
     raw_input()
