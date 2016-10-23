f0954716485aa56ad36874abb45173d1d5b3f071
Jaap Mengers <jaapm@q42.nl>
2016-10-23 11:27:51 +0200

    Implements detection of start screen

--- captureframes.py
+++ captureframes.py
@@ -1,34 +1,41 @@
 from PIL import Image
 import numpy as np
-import imageprocessing as ip
 from rx import Observable
 import sys
 import time
 
 def create_observable(observer):
     while True:
+        time.sleep(1/5)
         raw_image = sys.stdin.read(360*640*3)
 
         image = np.fromstring(raw_image, dtype='uint8')
         image = image.reshape((360,640,3))
 
-        p1winner = test(image, 207)
-        p2winner = test(image, 357)
+        if check_startscreen(image):
+            observer.on_next('startscreen')
+            continue
+
+        p1winner = check_winner(image, 207)
+        p2winner = check_winner(image, 357)
         if p1winner or p2winner:
             observer.on_next('winner is ' + ('p1' if p1winner else 'p2'))
-        else:
-            print 'No winner'
 
-        time.sleep(1)
 
-def test(input, x):
-    pixels = range(50, 117)
+def check_startscreen(input):
+    matrix = input[10,:][380:480]
+    diff = diff_to_baseline(matrix, [37, 51, 115])
 
-    matrix = input[:,x][50:117]
+    return diff < 8
 
+def diff_to_baseline(matrix, baseline):
     mean = np.mean(matrix, axis=0)
-    target = np.array([130, 111, 0])
-    diff = np.sum(np.absolute(mean - target))
+    target = np.array(baseline)
+    return np.sum(np.absolute(mean - target))
+
+def check_winner(input, x):
+    matrix = input[:,x][50:117]
+    diff = diff_to_baseline(matrix, [130, 111, 0])
 
     return diff < 8
 
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-livestreamer --http-header Client-ID=jzkbprff40iqj646a697cyrvl0zt2m6 -O http://www.twitch.tv/jaapem best | ffmpeg -i - -vf fps=1 -f image2pipe -pix_fmt rgb24 -vcodec rawvideo - | python main.py
+livestreamer --http-header Client-ID=jzkbprff40iqj646a697cyrvl0zt2m6 -O http://www.twitch.tv/jaapem best | ffmpeg -i - -vf fps=5 -f image2pipe -pix_fmt rgb24 -vcodec rawvideo - | python main.py
